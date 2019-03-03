34c78e0541d58d2e2ef5174097dc8285c1c01add
Alex Gochi <alex.gochi.javan@gmail.com>
2019-03-03 23:23:15 +0700

    Initial commit

---
+++ app/src/main/java/alexgochi/ruangguru/activity/MainActivity.java
@@ -0,0 +1,73 @@
+package alexgochi.ruangguru.activity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import alexgochi.ruangguru.R;
+import alexgochi.ruangguru.SessionHandler;
+import alexgochi.ruangguru.User;
+import alexgochi.ruangguru.fragment.AccountFragment;
+import alexgochi.ruangguru.fragment.HomeFragment;
+import alexgochi.ruangguru.fragment.OtherFragment;
+import alexgochi.ruangguru.fragment.PurchaseFragment;
+
+public class MainActivity extends AppCompatActivity implements
+        BottomNavigationView.OnNavigationItemSelectedListener {
+    private SessionHandler session;
+    private User user;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        session = new SessionHandler(getApplicationContext());
+        user = session.getUserDetails();
+
+        loadFragment(new HomeFragment());
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.navigation_purchased:
+                fragment = new PurchaseFragment();
+                break;
+            case R.id.navigation_account:
+                fragment = new AccountFragment();
+                break;
+            case R.id.navigation_other:
+                fragment = new OtherFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/alexgochi/ruangguru/fragment/HomeFragment.java
@@ -0,0 +1,36 @@
+package alexgochi.ruangguru.fragment;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import alexgochi.ruangguru.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return view;
+    }
+
+}
